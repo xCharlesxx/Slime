d6e0fc1db92223ce5905f4b8cf1841934f497465
Charlie <Charlie2.saunders@live.uwe.ac.uk>
2019-01-13 19:50:43 +0000

    Init

---
+++ Slime/Intermediate/Build/Win64/SlimeEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Charles/Documents/Unreal Projects/Slime/Intermediate/Build/Win64/SlimeEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ Slime/Intermediate/Build/Win64/UE4Editor/Inc/Slime/SlimeHUD.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SLIME_SlimeHUD_generated_h
+#error "SlimeHUD.generated.h already included, missing '#pragma once' in SlimeHUD.h"
+#endif
+#define SLIME_SlimeHUD_generated_h
+
+#define Slime_Source_Slime_SlimeHUD_h_12_RPC_WRAPPERS
+#define Slime_Source_Slime_SlimeHUD_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define Slime_Source_Slime_SlimeHUD_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASlimeHUD(); \
+	friend struct Z_Construct_UClass_ASlimeHUD_Statics; \
+public: \
+	DECLARE_CLASS(ASlimeHUD, AHUD, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Slime"), NO_API) \
+	DECLARE_SERIALIZER(ASlimeHUD)
+
+
+#define Slime_Source_Slime_SlimeHUD_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesASlimeHUD(); \
+	friend struct Z_Construct_UClass_ASlimeHUD_Statics; \
+public: \
+	DECLARE_CLASS(ASlimeHUD, AHUD, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Slime"), NO_API) \
+	DECLARE_SERIALIZER(ASlimeHUD)
+
+
+#define Slime_Source_Slime_SlimeHUD_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASlimeHUD(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASlimeHUD) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASlimeHUD); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASlimeHUD); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASlimeHUD(ASlimeHUD&&); \
+	NO_API ASlimeHUD(const ASlimeHUD&); \
+public:
+
+
+#define Slime_Source_Slime_SlimeHUD_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASlimeHUD(ASlimeHUD&&); \
+	NO_API ASlimeHUD(const ASlimeHUD&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASlimeHUD); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASlimeHUD); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ASlimeHUD)
+
+
+#define Slime_Source_Slime_SlimeHUD_h_12_PRIVATE_PROPERTY_OFFSET
+#define Slime_Source_Slime_SlimeHUD_h_9_PROLOG
+#define Slime_Source_Slime_SlimeHUD_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Slime_Source_Slime_SlimeHUD_h_12_PRIVATE_PROPERTY_OFFSET \
+	Slime_Source_Slime_SlimeHUD_h_12_RPC_WRAPPERS \
+	Slime_Source_Slime_SlimeHUD_h_12_INCLASS \
+	Slime_Source_Slime_SlimeHUD_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Slime_Source_Slime_SlimeHUD_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Slime_Source_Slime_SlimeHUD_h_12_PRIVATE_PROPERTY_OFFSET \
+	Slime_Source_Slime_SlimeHUD_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	Slime_Source_Slime_SlimeHUD_h_12_INCLASS_NO_PURE_DECLS \
+	Slime_Source_Slime_SlimeHUD_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Slime_Source_Slime_SlimeHUD_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
